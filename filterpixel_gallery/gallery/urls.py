f7988241aa5e48dbc23833491689858a6ef71c39
Harit <53652715+WindBlaze1@users.noreply.github.com>
2023-07-19 06:07:14 +0530

    fixed pagination bug

--- filterpixel_gallery/gallery/urls.py
+++ filterpixel_gallery/gallery/urls.py
@@ -4,8 +4,10 @@ from user_auth.views import signup_redirect
 
 urlpatterns = [
     # ('',),
-    path('gallery/social/signup',signup_redirect,name='signup_redirect'),
+    path('social/signup',signup_redirect,name='signup_redirect'),
     path('',views.show_gallery,name='gallery'),
+    path('drive/',views.show_gallery_drive,name='gallery_drive'),
+    path('s3/',views.show_gallery_s3,name='gallery_s3'),
     # path('display_images/',views.fetch_drive_images,name='drive_images'),
     path('',include('allauth.urls')),
 
--- filterpixel_gallery/gallery/views.py
+++ filterpixel_gallery/gallery/views.py
@@ -1,31 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 
 from gallery.helpers import get_drive_urls, get_s3_urls
 
 @login_required
+def show_gallery_drive(request):
+    images = get_drive_urls()
+    # print(images)
+    paginator = Paginator(images, 6)  # Number of images to display per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'gallery.html', {'page_obj': page_obj,'s3':'','drive':'active'})
+
+@login_required
+def show_gallery_s3(request):
+    images = get_s3_urls()
+    paginator = Paginator(images, 6)  # Number of images to display per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'gallery.html', {'page_obj': page_obj,'s3':'active','drive':''})
+
+@login_required
 def show_gallery(request):
-        print(request.method)
-        if request.method == 'POST':
-            button = request.POST.get('button')
-            print('selected:',button)
-            if button == 'drive':
-                images = get_drive_urls()
-                s3_active = ''
-                drive_active = 'active'
-            elif button == 's3':
-                images = get_s3_urls()
-                s3_active = 'active'
-                drive_active = ''
-        else:
-            images = get_s3_urls()
-            s3_active = 'active'
-            drive_active = ''
-
-
-        paginator = Paginator(images, 6)  # Number of images to display per page
-        page_number = request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
-
-        return render(request, 'gallery.html', {'page_obj': page_obj, 's3_active':s3_active,'drive_active':drive_active})
+    return redirect('gallery_drive')
